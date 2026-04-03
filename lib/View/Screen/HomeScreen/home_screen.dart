fb83699897b205dffac0498ec9c54899541e4c48
srsakilhossen302 <cse.sakilhossem@gmail.com>
2026-04-04 02:37:11 +0600

    feat: implement home screen UI with custom navigation bar and surah list view

--- lib/View/Screen/HomeScreen/home_screen.dart
+++ lib/View/Screen/HomeScreen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quran_international/View/Widget/CustomBottomNavBar/custom_bottom_nav_bar.dart';
 
 import '../../../../Utils/AppColors/app_colors.dart';
 import 'Controller/home_controller.dart';
@@ -39,6 +40,12 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        currentIndex: 0,
+        onTap: (index) {
+          // Handle navigation
+        },
       ),
     );
   }
---
+++ lib/View/Widget/CustomBottomNavBar/custom_bottom_nav_bar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90.h,
+      decoration: BoxDecoration(
+        color: const Color(0xFF0D1E15), // Matches the dark background from image
+        border: Border(
+          top: BorderSide(
+            color: Colors.white.withOpacity(0.05),
+            width: 1.w,
+          ),
+        ),
+      ),
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onTap,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFF22C55E), // Active green
+        unselectedItemColor: const Color(0xFF7F9285).withOpacity(0.8), // Inactive muted color
+        selectedLabelStyle: GoogleFonts.montserrat(
+          fontSize: 10.sp,
+          fontWeight: FontWeight.w700,
+          letterSpacing: 0.5,
+        ),
+        unselectedLabelStyle: GoogleFonts.montserrat(
+          fontSize: 10.sp,
+          fontWeight: FontWeight.w600,
+          letterSpacing: 0.5,
+        ),
+        items: [
+          _buildNavItem(
+            icon: 'assets/icons/Home_Icons-navbar.svg',
+            label: 'HOME',
+            index: 0,
+          ),
+          _buildNavItem(
+            icon: 'assets/icons/Search_Icons-navbar.svg',
+            label: 'SEARCH',
+            index: 1,
+          ),
+          _buildNavItem(
+            icon: 'assets/icons/Bookmarks_Icons-navbar.svg',
+            label: 'BOOKMARKS',
+            index: 2,
+          ),
+          _buildNavItem(
+            icon: 'assets/icons/Highlights_Icons-navbar.svg',
+            label: 'HIGHLIGHTS',
+            index: 3,
+          ),
+          _buildNavItem(
+            icon: 'assets/icons/Settings_Icons-navbar.svg',
+            label: 'SETTINGS',
+            index: 4,
+          ),
+        ],
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavItem({
+    required String icon,
+    required String label,
+    required int index,
+  }) {
+    final bool isSelected = currentIndex == index;
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: EdgeInsets.only(bottom: 5.h),
+        child: SvgPicture.asset(
+          icon,
+          width: 24.w,
+          height: 24.w,
+          colorFilter: ColorFilter.mode(
+            isSelected ? const Color(0xFF22C55E) : const Color(0xFF7F9285),
+            BlendMode.srcIn,
+          ),
+        ),
+      ),
+      label: label,
+    );
+  }
+}
